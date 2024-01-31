a354012317d5fb98f7f88495d8351a97f0281916
Quoc Nguyen <qnguyen523@gmail.com>
2024-01-31 18:26:34 +0700

    save

---
+++ udemy_react/01-starting-project/src/App.jsx
@@ -0,0 +1,10 @@
+import ProjectsSidebar from './components/ProjectsSidebar.jsx';
+function App() {
+  return (
+    <main className="h-screen my-8">
+      <ProjectsSidebar />
+    </main>
+  );
+}
+
+export default App;
